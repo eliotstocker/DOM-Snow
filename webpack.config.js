1d4f512328b65b504a7d9cf6a26cb8463a0bc3c0
Eliot Stocker <estocker@brandwatch.com>
2020-12-10 10:24:58 +0000

    ci: add babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,20 @@ module.exports = {
     target: 'web',
     mode: 'production',
     entry: './js/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //Regular expression
+                exclude: /(node_modules)/,//excluded node_modules
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]  //Preset used for env setup
+                    }
+                }
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.min.js',
